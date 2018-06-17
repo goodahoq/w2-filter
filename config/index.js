ee47be5c7bd4a410ec0e24ad0da8cc2d4da07299
goodahoq <qqused850805@gmail.com>
2018-06-17 14:56:26 +0800

    public path

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/w2-filter/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/w2-filter/',
 
     /**
      * Source Maps
