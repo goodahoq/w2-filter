242d71736cb568fe34221ded771cc4df45245fe3
goodahoq <qqused850805@gmail.com>
2018-06-17 15:18:47 +0800

    BUILD

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './w2-filter',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
